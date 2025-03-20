cfd3dbfeec7a11b997f911166ae3a6b1ffbb3ebc
Rogerio Oliveira <rogeduardo@hotmail.com>
2025-03-20 18:55:19 -0300

    Add a method to remove invalid character for glob method pattern and do some improvement to keyword search in the files.

--- logfind.c
+++ logfind.c
@@ -7,38 +7,68 @@
 
 #define MAX_LINE 1024
 
+void removeinvalidchar (char *str, char invchar)
+{
+	char *pos = strchr( str, invchar );
+	if (pos)
+	{
+		debug("Found a invalid character in %s at %ld\n", str, (pos - str) );
+		*pos = '\0';
+	}
+}
+
 void logfind (char **keywords, int keywordsc, bool operator)
 {
 	FILE *flog=NULL, *fpattern=NULL;
 	char lineflog[MAX_LINE], linefpattern[MAX_LINE], *token, *home;
-	glob_t ret_glob;
+	char invalid_character[3] = "\r\n";
 	int out;
+	glob_t ret_glob;
 
 	home = getenv ("HOME");
-	flog = fopen ( strcat(home, "/.logfind"), "r" );
+	flog = fopen (strcat(home, "/.logfind"), "r");
 	check (flog, "Failed to open $HOME/.logfind");
 	while (fgets (lineflog, MAX_LINE, flog))
 	{
+		/*Remove all invalid characters (\r or \n) from the string to be used on glob,
+		 *because it take them in consider in a search */
+		for (int i=0; i < strlen (invalid_character); i++)
+		{
+			removeinvalidchar (lineflog, invalid_character[i]);
+		}
+
 		out = glob (lineflog, 0, NULL, &ret_glob);
-		debug("Return glob %d of pattern file %s\n", out, lineflog);
+		debug("Return glob %d of pattern file %s and found %zu file(s)\n", out, lineflog, ret_glob.gl_pathc);
+		free (token);
+
 		if (!out)
 		{
-			for (int j=0; j < ret_glob.gl_pathc; j++)
+			for (int i=0; i < ret_glob.gl_pathc; i++)
 			{
-				debug ("Search on this file %s\n", ret_glob.gl_pathv[j]);
-				fpattern = fopen (ret_glob.gl_pathv[j], "r");
-				for (int i=0; i < keywordsc; i++ )
+				debug ("Search on this file %s\n", ret_glob.gl_pathv[i]);
+				fpattern = fopen (ret_glob.gl_pathv[i], "r");
+				check (fpattern, "Failed to open %s\n", ret_glob.gl_pathv[i]);
+				int line = 1;
+				while( fgets (linefpattern, MAX_LINE, fpattern) )
 				{
-					token = strtok (keywords[i], " ");
-					while (!token)
+					for (int j=0; j < keywordsc; j++ )
 					{
-						printf ("%s\n", token);
-						token = strtok (NULL, " ");
+						token = strtok (linefpattern, " ");
+						while (token)
+						{
+							debug("%s\n", token);
+							if ( !strcmp ( token, keywords[j] ) )
+								printf ("Found a keyword %s at %s, line %d!\n", keywords[j], ret_glob.gl_pathv[i], line );
+
+							token = strtok (NULL, " ");
+						}
+						free (token);
 					}
-					free (token);
+					line++;
 				}
 			}
 		}
+		globfree (&ret_glob);
 	}
 
 	error:
